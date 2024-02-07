ebabaeeae325ebeb667334b1aa70d1e3bae9bb1e
Evelyn <evelyn-johannsen@outlook.de>
2024-02-07 16:02:20 +0100

    2. commit. A funciton is added

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 print("Welcome to my Python-Test!")
 name = input("Tell me your name: ")
 print("Hey,", name)
+
+def check_name_length(name):
+    if len(name) > 10:
+        print("Wow, your name is quite long!")
+    else:
+        print("Your name has a good length.")
